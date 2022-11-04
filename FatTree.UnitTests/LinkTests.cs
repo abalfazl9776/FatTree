f853fb26f5e5086d675b8e31999d5c86181d3aae
Abolfazl Vayani <avhb2lr@hotmail.com>
2022-11-04 12:02:23 +0330

    add unit test for k = 8

--- FatTree.UnitTests/LinkTests.cs
+++ FatTree.UnitTests/LinkTests.cs
@@ -3,7 +3,7 @@ namespace FatTree.UnitTests;
 public class LinkTests
 {
     [Fact]
-    public void BuildNetwork_KIs4_ThereShouldBe36Links()
+    public void BuildNetwork_KIs4_ThereShouldBe48Links()
     {
         // Arrange
         byte k = 4;
@@ -15,6 +15,27 @@ public class LinkTests
 
         // Assert
         Assert.Equal(48, links.Count);
+        foreach (var link in links)
+        {
+            Assert.Contains(expectedLinks, x =>
+                (x.Node_1 == link.Node_1 || x.Node_1 == link.Node_2)
+                &&
+                (x.Node_2 == link.Node_1 || x.Node_2 == link.Node_2));
+        }
+    }
+    [Fact]
+    public void BuildNetwork_KIs4_ThereShouldBe384Links()
+    {
+        // Arrange
+        byte k = 8;
+        var expectedLinks = TestData.K8Links;
+
+        // Act
+        var dataCenter = new FatTreeDataCenter(k);
+        var links = dataCenter.GetLinks().ToList();
+
+        // Assert
+        Assert.Equal(384, links.Count);
         foreach (var link in links)
         {
             Assert.Contains(expectedLinks, x =>
